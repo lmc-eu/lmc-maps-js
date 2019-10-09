a6203598fd0011e10d0c8bb268d3a6f79dab8f59
Jiří Hykel <jiri.hykel@lmc.eu>
2019-10-09 15:47:56 +0200

    Remove minZoom parameter

--- src/lib/lmc-maps.js
+++ src/lib/lmc-maps.js
@@ -30,7 +30,6 @@ class LmcMaps {
         this.map = new mapboxgl.Map({
             container: this.container,
             style: this.style,
-            minZoom: 6,
             center: this.coords,
             zoom: this.zoom,
             renderWorldCopies: false,
